2a53298f225c0b825f624902588bd53e592a3f87
abayomi igwubor <igwubor@outlook.com>
2020-05-23 19:31:08 +0100

    Add project files.

---
+++ WebgentleBookStore/Startup.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace WebgentleBookStore
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //app.Use(async( context, next) =>
+            //{
+            //    await context.Response.WriteAsync("Hello from my first middleware");
+            //    await next();
+            //    await context.Response.WriteAsync("Hello from my first middleware: response again");
+            //});
+
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("Hello from my second middleware");
+            //    await next();
+            //    await context.Response.WriteAsync("Hello from my second middleware: response again");
+            //});
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("Hello from my third middleware");
+            //    await next();
+              
+            //});
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+                //("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello World!");
+                //});
+            });
+           // app.UseRouting();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.Map("/yomi", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello World! yomi deve");
+            //    });
+            //});
+        }
+    }
+}
