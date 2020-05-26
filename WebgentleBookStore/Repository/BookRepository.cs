287f36ebf94a4ea0b829aa37f318e5c930c59a01
abayomi igwubor <igwubor@outlook.com>
2020-05-26 10:08:13 +0100

    added functionality for each book

--- WebgentleBookStore/Repository/BookRepository.cs
+++ WebgentleBookStore/Repository/BookRepository.cs
@@ -27,12 +27,12 @@ namespace WebgentleBookStore.Repository
         {
             return new List<BookModel>()
             {
-                new BookModel (){Id = 1, Title="MVC", Author="yomi", Description="The MVC Dot net Book"},
-                 new BookModel (){Id = 2, Title="Python", Author="seun",Description="All you need to know about the snake"},
-                  new BookModel (){Id = 3, Title="JavaScript", Author="kunle",Description="Better than eloquent javascript"},
-                   new BookModel (){Id = 4, Title="Php", Author="yomi",Description="The best backendless technology"},
-                    new BookModel (){Id = 5, Title="Java", Author="mike",Description="The highly respected java"},
-                    new BookModel (){Id = 6, Title="AzureDevops", Author="williams",Description="The best book on DevOps"}
+                new BookModel (){Id = 1, Title="MVC", Author="yomi", Description="The MVC Dot net Book",Category="Programming",Language="English",TotalPages=234},
+                 new BookModel (){Id = 2, Title="Python", Author="seun",Description="All you need to know about the snake",Category="Framework",Language="Spainish",TotalPages=24},
+                  new BookModel (){Id = 3, Title="JavaScript", Author="kunle",Description="Better than eloquent javascript",Category="Developer",Language="English",TotalPages=134},
+                   new BookModel (){Id = 4, Title="Php", Author="yomi",Description="The best backendless technology",Category="Concept",Language="French",TotalPages=44},
+                    new BookModel (){Id = 5, Title="Java", Author="mike",Description="The highly respected java",Category="DevOps",Language="English",TotalPages=114},
+                    new BookModel (){Id = 6, Title="AzureDevops", Author="williams",Description="The best book on DevOps",Category="Programming",Language="English",TotalPages=321}
             };
         }
     }   
