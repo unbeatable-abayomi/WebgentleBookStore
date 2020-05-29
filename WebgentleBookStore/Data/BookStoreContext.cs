01ef2c25bdad6c3837e9c49aa203571a397ca74e
abayomi igwubor <igwubor@outlook.com>
2020-05-29 16:40:45 +0100

    added Db context

---
+++ WebgentleBookStore/Data/BookStoreContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebgentleBookStore.Data
+{
+    public class BookStoreContext : DbContext
+    {
+        public BookStoreContext(DbContextOptions<BookStoreContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Books> Books { get; set; }
+
+        //protected override  void OnConfiguring(DbContextOptionsBuilder opitonBuilder)
+        //{
+        //    opitonBuilder.UseSqlServer("Server=.;Database=BookStore;Integrated Security=True;");
+        //        base.OnConfiguring(opitonBuilder);
+        //}
+    }
+}
